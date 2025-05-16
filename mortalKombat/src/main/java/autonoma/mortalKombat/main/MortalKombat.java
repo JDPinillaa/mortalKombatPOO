ac60af4612d0a2821be896efda9cf0a65effe7b0
Juan Diego Pinilla <juand.pinillaa@autonoma.edu.co>
2025-05-16 17:44:25 -0500

    proyecto creado

---
+++ mortalKombat/src/main/java/autonoma/mortalKombat/main/MortalKombat.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package autonoma.mortalKombat.main;
+
+/**
+ *
+ * @author juand
+ */
+public class MortalKombat {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
